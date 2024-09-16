71893a4ce4f2aeda038cc50855b9cb701a93bdbf
Tyler Wall <Tyler.wall@keyin.com>
2024-09-16 14:40:42 -0230

    Initial Commit

--- QAP1/password-generator.js
+++ QAP1/password-generator.js
@@ -47,7 +47,7 @@ function helpMessage() {
         --help          Show help message
     
     Example:
-         password-generator --length 8 --symbols -numbers
+         node password-generator --length 8 --symbols -numbers
         ^ This should generator your desired password
          `);
 }
