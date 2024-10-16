1c3c2fc54f45afd348794ec43714706ef86e5389
Tyler Wall <Tyler.wall@keyin.com>
2024-10-16 18:37:26 -0230

    todo example + start of library one

--- postgres-backend-examples-main/library-book-management-app/index.js
+++ postgres-backend-examples-main/library-book-management-app/index.js
@@ -1,11 +1,11 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 // PostgreSQL connection
 const pool = new Pool({
-  user: 'postgres', //This _should_ be your username, as it's the default one Postgres uses
-  host: 'localhost',
-  database: 'keyin_test', //This should be changed to reflect your actual database
-  password: 'Keyin123', //This should be changed to reflect the password you used when setting up Postgres
+  user: "postgres", //This _should_ be your username, as it's the default one Postgres uses
+  host: "localhost",
+  database: "postgres", //This should be changed to reflect your actual database
+  password: "wall.11", //This should be changed to reflect the password you used when setting up Postgres
   port: 5432,
 });
 
@@ -13,54 +13,60 @@ const pool = new Pool({
  * Create the books table if it does not already exist
  */
 async function createTable() {
+  await pool.query(`
+  CREATE TABLE IF NOT EXISTS books (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  author TEXT NOT NULL,
+  checked_out BOOLEAN DEFAULT FALSE)`);
 }
 
 /**
  * Adds a new book to the library
- * 
+ *
  * @param {string} title - The title of the book
  * @param {string} author - The author of the book's name
  */
 async function addBook(title, author) {
+  await pool.query("INSERT INTO books (title, author) VALUES ($1, $2)", [
+    title,
+    author,
+  ]);
+  console.log(`Added "${title}" by "${author}" to the library`);
 }
 
 /**
  * List out all books in the library (both checked and not checked out)
  */
-async function showBooks() {
-}
+async function showBooks() {}
 
 /**
  * Update a given book's author or title
- * 
+ *
  * @param {number} id - The ID of the book to update
  * @param {string} field - The field to update (author or title)
  * @param {string} newValue - The new value of the field
  */
-async function updateBook(id, field, newValue) {
-}
+async function updateBook(id, field, newValue) {}
 
 /**
  * Check out a book from the library
- * 
+ *
  * @param {number} id - The ID of the book to checkout
  */
-async function checkoutBook(id) {
-}
+async function checkoutBook(id) {}
 
 /**
  * Remove a book from the library
- * 
+ *
  * @param {number} id - The ID of the book to remove
  */
-async function deleteBook(id) {
-}
+async function deleteBook(id) {}
 
 /**
  * Print out all books currently available to be checked out from the library
  */
-async function availableBooks() {
-}
+async function availableBooks() {}
 
 /**
  * Runs the CLI app to interact with the library
@@ -72,42 +78,42 @@ async function runCLI() {
   const command = args[0];
 
   switch (command) {
-    case 'add':
+    case "add":
       const [title, author] = args.slice(1);
       await addBook(title, author);
       break;
-    case 'show':
+    case "show":
       await showBooks();
       break;
-    case 'update':
+    case "update":
       const [id, field, newValue] = args.slice(1);
-      if (field === 'title' || field === 'author') {
+      if (field === "title" || field === "author") {
         await updateBook(id, field, newValue);
-      }
-      else {
+      } else {
         console.log('Invalid field. Only "title" or "author" can be updated.');
       }
       break;
-    case 'checkout':
+    case "checkout":
       const checkoutId = args[1];
       await checkoutBook(checkoutId);
       break;
-    case 'delete':
+    case "delete":
       const deleteId = args[1];
       await deleteBook(deleteId);
       break;
-    case 'available':
+    case "available":
       await availableBooks();
       break;
     default:
-      console.log('Invalid command. Available commands: add, show, update, checkout, delete, available.');
+      console.log(
+        "Invalid command. Available commands: add, show, update, checkout, delete, available."
+      );
   }
 
   await pool.end();
 }
 
-runCLI()
-  .catch((error) => {
-    console.error('Error executing query', error.stack);
-    return pool.end();
-  });
+runCLI().catch((error) => {
+  console.error("Error executing query", error.stack);
+  return pool.end();
+});
--- postgres-backend-examples-main/todo-list-app/index.js
+++ postgres-backend-examples-main/todo-list-app/index.js
@@ -1,11 +1,11 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 // PostgreSQL connection
 const pool = new Pool({
-  user: 'postgres', //This _should_ be your username, as it's the default one Postgres uses
-  host: 'localhost',
-  database: 'keyin_test', //This should be changed to reflect your actual database
-  password: 'Keyin123', //This should be changed to reflect the password you used when setting up Postgres
+  user: "postgres", //This _should_ be your username, as it's the default one Postgres uses
+  host: "localhost",
+  database: "postgres", //This should be changed to reflect your actual database
+  password: "wall.11", //This should be changed to reflect the password you used when setting up Postgres
   port: 5432,
 });
 
@@ -13,49 +13,62 @@ const pool = new Pool({
  * Creates the to do list table, if it does not already exist
  */
 async function createTable() {
-
-};
+  await pool.query(
+    `CREATE TABLE IF NOT EXISTS todos (
+    id SERIAL PRIMARY KEY,
+    task TEXT NOT NULL,
+    completed BOOLEAN DEFAULT FALSE
+    )`
+  );
+}
 
 /**
  * Adds a new todo list item
- * 
+ *
  * @param {string} task - The item to add
  */
 async function addTodo(task) {
-
-};
+  await pool.query("INSERT INTO todos (task) VALUES ($1)", [task]);
+}
 
 /**
  * Prints all todo list items to the console
  */
 async function showTodos() {
+  const response = await pool.query("SELECT * FROM todos");
 
-};
+  response.rows.forEach((row) => {
+    console.log(`${row.id}: ${row.task}: ${row.completed}`);
+  });
+}
 
 /**
  * Marks the specified todo list item as completed
- * 
+ *
  * @param {number} id - The ID of the todo list item
  */
 async function completeTodo(id) {
-
-};
+  await pool.query("UPDATE todos SET completed = TRUE WHERE id = $1", [id]);
+  console.log(`Marked item ${id} as completed `);
+}
 
 /**
  * Deletes the specified todo list item
- * 
+ *
  * @param {number} id - The ID of the todo list item
  */
 async function deleteTodo(id) {
-
-};
+  await pool.query("DELETE FROM todos WHERE id = $1", [id]);
+  console.log(`Successfully deleted item ${id}`);
+}
 
 /**
  * Removes all todo list items that have been completed
  */
 async function clearCompleted() {
-
-};
+  await pool.query("DELETE FROM todos WHERE completed = TRUE");
+  console.log("Successfully removed all completed tasks.");
+}
 
 /**
  * Runs our CLI app to manage the todo list
@@ -65,34 +78,35 @@ async function runCLI() {
   const args = process.argv.slice(2);
 
   switch (args[0]) {
-    case 'add':
-      const task = args.slice(1).join(' ');
+    case "add":
+      const task = args.slice(1).join(" ");
       await addTodo(task);
       break;
-    case 'show':
+    case "show":
       await showTodos();
       break;
-    case 'complete':
+    case "complete":
       const completeId = parseInt(args[1], 10);
       await completeTodo(completeId);
       break;
-    case 'delete':
+    case "delete":
       const deleteId = parseInt(args[1], 10);
       await deleteTodo(deleteId);
       break;
-    case 'clear':
+    case "clear":
       await clearCompleted();
       break;
     default:
-      console.log('Invalid command. Use add, show, complete, delete, or clear.');
+      console.log(
+        "Invalid command. Use add, show, complete, delete, or clear."
+      );
   }
 
   // Close the database connection
   await pool.end();
-};
+}
 
-runCLI()
-  .catch((error) => {
-    console.error('Error executing query', error.stack);
-    return pool.end();
-  });
+runCLI().catch((error) => {
+  console.error("Error executing query", error.stack);
+  return pool.end();
+});
